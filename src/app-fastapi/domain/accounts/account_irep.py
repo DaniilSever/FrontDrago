29ad3c2c14db97c44fe18f3b9d07f28b92108213
Daniil <jhisjfiohig14@mail.ru>
2024-08-11 21:30:24 +0700

    init irep

--- src/app-fastapi/domain/accounts/account_irep.py
+++ src/app-fastapi/domain/accounts/account_irep.py
@@ -1,5 +1,7 @@
 from typing import Protocol
 from pydantic import UUID4
+from dto import QCreateAccount, QUpdateAccount
+from dto import ZOk, ZError
 
 class IrepAccount(Protocol):
     
@@ -9,5 +11,11 @@ class IrepAccount(Protocol):
     def get_account_by_id(self, account_id: UUID4) -> dict:
         raise NotImplementedError
     
-    def create_account(self, account_data: dict) -> dict:
+    def create_account(self, req: QCreateAccount) -> ZOk | ZError:
+        raise NotImplementedError
+    
+    def update_account(self, account_id: UUID4, req: QUpdateAccount) -> ZOk | ZError:
+        raise NotImplementedError
+    
+    def delete_account(self, account_id: UUID4) -> ZOk | ZError:
         raise NotImplementedError
